15b1b3ff0de15cd2076358d438676cab3f6b7f51
xuejiaming <326308290@qq.com>
2023-03-21 21:57:27 +0800

    1

--- deploy.sh
+++ deploy.sh
@@ -23,6 +23,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 # 把上面的 <USERNAME> 换成你自己的 Github 用户名，<REPO> 换成仓库名，比如我这里就是：
-git push -f git@github.com:xuejmnet/sharding-core-doc.git main:gh-pages
+git push -f git@github.com:xuejmnet/easy-query-doc.git main:gh-pages
 
 cd -
