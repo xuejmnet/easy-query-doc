ab9f460cf7493c7707e1258710d7ea602b340c1a
xuejiaming <326308290@qq.com>
2023-08-19 14:34:20 +0800

    1

--- src/.vuepress/sidebar/zh.ts
+++ src/.vuepress/sidebar/zh.ts
@@ -23,7 +23,7 @@ export const zhSidebar = sidebar({
         {
           text: "查询",
           prefix: "query/",
-          children: ["readme.md","or-condition","paging","multi-query","group","select","native-sql","dynamic-table","stream-query","basic-type","to-map","select-column","union","select-sub","sub-query","relation","fill","where","reuse","case-when","client-dynaimic-where","client-dynamic-sort"],
+          children: ["readme.md","or-condition","paging","multi-query","group","select","native-sql","dynamic-table","stream-query","basic-type","to-map","select-column","union","select-sub","sub-query","relation","fill","where","reuse","case-when","dynaimic-where","dynamic-sort"],
         },
         {
           text: "高级",
