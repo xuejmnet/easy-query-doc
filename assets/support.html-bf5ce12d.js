c95da46ded354577b94c9276cd72cbe748b20f5f
xuejmnet <326308290@qq.com>
2023-12-29 16:21:21 +0000

    Deploying to gh-pages from @ xuejmnet/easy-query-doc@5618754d2e55c3881d420ec3c6bc565c16889049 🚀

---
+++ assets/support.html-bf5ce12d.js
@@ -0,0 +1,11 @@
+import{_ as r}from"./plugin-vue_export-helper-c27b6911.js";import{r as s,o as a,c,a as e,b as t,d as n,f as l}from"./app-e5037745.js";const _="/easy-query-doc/wx.jpg",d="/easy-query-doc/zfb.jpg",i={},h=e("h1",{id:"支持",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#支持","aria-hidden":"true"},"#"),t(" 支持")],-1),p=e("p",null,[t("您的支持是我坚持的动力,在这里希望您可以免费给我点个"),e("code",null,"star")],-1),m={href:"https://github.com/xuejmnet/easy-query",target:"_blank",rel:"noopener noreferrer"},u={href:"https://gitee.com/xuejm/easy-query",target:"_blank",rel:"noopener noreferrer"},f=e("h2",{id:"爱心",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#爱心","aria-hidden":"true"},"#"),t(" 爱心")],-1),x=e("p",null,"如果您觉得这个框架有用可以请作者喝杯咖啡",-1),b=e("img",{src:_,class:"no-zoom",style:{width:"200px"}},null,-1),y=e("img",{src:d,class:"no-zoom",style:{width:"200px"}},null,-1);function g(k,E){const o=s("ExternalLinkIcon");return a(),c("div",null,[h,p,e("p",null,[e("a",m,[t("GITHUB"),n(o)])]),e("p",null,[e("a",u,[t("GITEE"),n(o)])]),f,x,b,y,l(` 
+::: code-tabs
+@tab 对象模式
+
+@tab lambda模式
+@tab proxy模式
+@tab 属性模式
+
+
+::: 
+`)])}const B=r(i,[["render",g],["__file","support.html.vue"]]);export{B as default};
